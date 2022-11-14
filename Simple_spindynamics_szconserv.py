1835c465e769f92ecc2a6d1ba889bc07bab971e7
tshimokaw <tokuro-shimokawa@oist.jp>
2022-11-14 21:02:24 +0900

    Update Simple_spindynamics_szconserv.py

--- Simple_spindynamics_szconserv.py
+++ Simple_spindynamics_szconserv.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 import math
 import numpy as np
-#import scipy.linalg
 import scipy.sparse
 import scipy.sparse.linalg
 import argparse
@@ -39,7 +38,7 @@ def init_parameters(N,Sz):
     iup = (1<<(N-Nup))-1   #all up state from 0-th to (N-Nup)-th site
     return Nup, Nhilbert, ihfbit, irght, ilft, iup
 
-def make_list(N,Nup,Nhilbert,ihfbit,irght,ilft,iup):
+def make_list(Nup,Nhilbert,ihfbit,irght,ilft,iup):
     list_1 = np.zeros(Nhilbert,dtype=int)
     list_ja = np.zeros(ihfbit,dtype=int)
     list_jb = np.zeros(ihfbit,dtype=int)
@@ -52,7 +51,7 @@ def make_list(N,Nup,Nhilbert,ihfbit,irght,ilft,iup):
     list_ja[ia_old] = ja
     list_jb[ib_old] = jb
     ii = snoob(ii)
-    for i in range(1,Nhilbert):
+    for n in range(1,Nhilbert):
         ia = ii & irght
         ib = (ii & ilft) // ihfbit
         if (ib == ib_old):
@@ -60,7 +59,7 @@ def make_list(N,Nup,Nhilbert,ihfbit,irght,ilft,iup):
         else:
             jb += ja+1
             ja = 0
-        list_1[i] = ii
+        list_1[n] = ii
         list_ja[ia] = ja
         list_jb[ib] = jb
         ia_old = ia
@@ -76,28 +75,28 @@ def get_ja_plus_jb(ii,irght,ilft,ihfbit,list_ja,list_jb):
     return ja+jb
 
 
-def ham_to_vec_complex(w,v1,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb):
+def ham_to_vec_complex(w,v1,Jxx,Jzz,list_isite1,list_isite2,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb):
     w = np.zeros(Nhilbert,dtype=complex)
 
-    for i in range(Nhilbert):
-       ii = list_1[i]        # i-th basis   
-       for k in range(Nint): # loop for all interaction
-        isite1 = list_isite1[k]
-        isite2 = list_isite2[k]
+    for n in range(Nhilbert):
+       ii = list_1[n]          # n-th basis   
+       for ij in range(Nint):  # loop for all interaction
+        isite1 = list_isite1[ij]
+        isite2 = list_isite2[ij]
         is1 = 1<<isite1
         is2 = 1<<isite2
         is12 = is1 + is2
-        wght = 2.0*Jxx[k]
-        diag = Jzz[k]
+        wght = 2.0*Jxx[ij]
+        diag = Jzz[ij]
         ibit = ii & is12
 
         if (ibit==0 or ibit==is12): #up-up or down-down
-             w[i] += diag*v1[i]
+             w[n] += diag*v1[n]
         else:  
-            w[i] -= diag*v1[i]
+            w[n] -= diag*v1[n]
             iexchg = ii ^ is12 
             newcfg = get_ja_plus_jb(iexchg,irght,ilft,ihfbit,list_ja,list_jb)
-            w[i] += wght*v1[newcfg] 
+            w[n] += wght*v1[newcfg] 
     return w/4.0 
 
 def continued_fraction(norm,salphas,sbetas,itr_dsf,eta,minene):
@@ -128,8 +127,8 @@ def sz_spin_dynamics(itr_dsf,qx,GS,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbe
     v0 = np.zeros(Nhilbert, dtype=complex)
     w = np.zeros(Nhilbert,dtype=complex)
     tmp = np.zeros(2,dtype=float)
-    for i in range(Nhilbert):
-         ii = list_1[i]
+    for n in range(Nhilbert):
+         ii = list_1[n]
          tmp[0:2]=0.0
 
          for k in range(N):
@@ -143,7 +142,7 @@ def sz_spin_dynamics(itr_dsf,qx,GS,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbe
               tmp[0] -= np.cos(qx*k)
               tmp[1] -= np.sin(qx*k)   
 
-         v1[i] = GS[i]*tmp[0]/2.0 + 1.0j*GS[i]*tmp[1]/2.0 
+         v1[n] = GS[n]*tmp[0]/2.0 + 1.0j*GS[n]*tmp[1]/2.0 
 
     v1 = v1/np.sqrt(N)
     norm = np.vdot(v1,v1)
@@ -153,7 +152,7 @@ def sz_spin_dynamics(itr_dsf,qx,GS,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbe
     sbeta=0. 
 
     for itr in range(0,itr_dsf):
-        w = ham_to_vec_complex(w,v1,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb)
+        w = ham_to_vec_complex(w,v1,Jxx,Jzz,list_isite1,list_isite2,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb)
         salpha = np.vdot(v1,w)
         w = w - salpha*v1 -sbeta*v0
         v0 = np.copy(v1)
@@ -164,28 +163,28 @@ def sz_spin_dynamics(itr_dsf,qx,GS,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbe
 
     return salphas, sbetas, norm
 
-def ham_to_vec(w,v1,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb):
-    w = np.zeros(Nhilbert,dtype=float)
+def ham_to_vec(w,v1,Jxx,Jzz,list_isite1,list_isite2,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb):
+    w = np.zeros(Nhilbert,dtype=float) #output vector
 
-    for i in range(Nhilbert):
-       ii = list_1[i]        # i-th basis   
-       for k in range(Nint): # loop for all interaction
-        isite1 = list_isite1[k]
-        isite2 = list_isite2[k]
+    for n in range(Nhilbert):
+       ii = list_1[n]           # n-th basis   
+       for ij in range(Nint):    # loop for all interaction
+        isite1 = list_isite1[ij]  # site i
+        isite2 = list_isite2[ij]  # site j
         is1 = 1<<isite1
         is2 = 1<<isite2
-        is12 = is1 + is2
-        wght = 2.0*Jxx[k]
-        diag = Jzz[k]
+        is12 = is1 + is2       # up-up state only at site i and j
+        wght = 2.0*Jxx[ij]
+        diag = Jzz[ij]
         ibit = ii & is12
 
         if (ibit==0 or ibit==is12): #up-up or down-down
-             w[i] += diag*v1[i]
+             w[n] += diag*v1[n]
         else:  
-            w[i] -= diag*v1[i]
+            w[n] -= diag*v1[n]
             iexchg = ii ^ is12 
             newcfg = get_ja_plus_jb(iexchg,irght,ilft,ihfbit,list_ja,list_jb)
-            w[i] += wght*v1[newcfg] 
+            w[n] += wght*v1[newcfg] 
     return w
 
 
@@ -202,8 +201,8 @@ def simple_lanczos(Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ih
     beta = 0.
     pre_energy=0
     
-    for iteration in range(0, itr_max):
-        w = ham_to_vec(w,v1,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb)
+    for k in range(0, itr_max):
+        w = ham_to_vec(w,v1,Jxx,Jzz,list_isite1,list_isite2,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb)
         alpha = np.dot(v1,w)
         w = w -alpha*v1 -beta*v0
         v0 = np.copy(v1)
@@ -216,8 +215,8 @@ def simple_lanczos(Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ih
         print(min(t_eigs)/4)
 
         if np.abs(min(t_eigs)-pre_energy) < eps:
-          print("Lanczos converged in", iteration, "iterations")
-          conv_itr=iteration
+          print("Lanczos converged in", k, "iterations")
+          conv_itr=k #M value in the tutorial slide
           print("The lowest 5 energy", t_eigs[0:4]/4)
           break
 
@@ -225,7 +224,7 @@ def simple_lanczos(Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ih
 
 
     #calcu GS eigenvector
-    np.random.seed(seed=12345)
+    np.random.seed(seed=12345)    #set the same seed value we used above
     v1 = np.random.rand(Nhilbert)
     v1 /= np.linalg.norm(v1)
     v0 = np.zeros(Nhilbert, dtype=float)
@@ -235,22 +234,22 @@ def simple_lanczos(Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ih
           
     GS = t_vecs[0,0]*v1  # GS wavefunction from 0-th eigenvector of the tridiagonal matrix
 
-    for itr in range(0,conv_itr-1):
-        w = ham_to_vec(w,v1,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb)
+    for k in range(0,conv_itr-1):
+        w = ham_to_vec(w,v1,Jxx,Jzz,list_isite1,list_isite2,Nint,Nhilbert,irght,ilft,ihfbit,list_1,list_ja,list_jb)
         alpha = np.dot(v1,w)
         w = w -alpha*v1 -beta*v0
         v0 = np.copy(v1)
         beta = np.sqrt(np.dot(w,w))
         v1 = w/beta
 
-        GS = GS + t_vecs[itr+1,0]*v1
-        print("eigenvector iteretion", itr)
+        GS = GS + t_vecs[k+1,0]*v1
+        print("eigenvector iteretion", k)
 
 
     return t_eigs, np.array(alphas), np.array(betas[1:]), t_vecs, GS
 
 
-def make_lattice_chain(N,J1,J2):
+def make_lattice_chain(N,J1,J2): #J1-J2 chain
     Jxx = []
     Jzz = []
     list_isite1 = []
@@ -275,53 +274,14 @@ def make_lattice_chain(N,J1,J2):
     return Jxx, Jzz, list_isite1, list_isite2, Nint
 
 
-def make_lattice(Lx,Ly,J1,J2):
-    Jxx = []
-    Jzz = []
-    list_isite1 = []
-    list_isite2 = []
-    Nint = 0
-    for iy in range(Ly):
-        for ix in range(Lx):
-            site1 = ix + Lx*iy
-            site1x = (ix+1)%Lx + Lx*iy
-            site1y = ix + Lx*((iy+1)%Ly)
-            site1xpy = (ix+1)%Lx + Lx*((iy+1)%Ly)
-            site1xmy = (ix+1)%Lx + Lx*((iy-1+Ly)%Ly)
-#
-            list_isite1.append(site1)
-            list_isite2.append(site1x)
-            Jxx.append(J1)
-            Jzz.append(J1)
-            Nint += 1
-#
-            list_isite1.append(site1)
-            list_isite2.append(site1y)
-            Jxx.append(J1)
-            Jzz.append(J1)
-            Nint += 1
-#
-            list_isite1.append(site1)
-            list_isite2.append(site1xpy)
-            Jxx.append(J2)
-            Jzz.append(J2)
-            Nint += 1
-#
-            list_isite1.append(site1)
-            list_isite2.append(site1xmy)
-            Jxx.append(J2)
-            Jzz.append(J2)
-            Nint += 1
-    return Jxx, Jzz, list_isite1, list_isite2, Nint
-
 def main():
     N=10
     Sz=0
     J1=1.00
     J2=0.4
-    qx=np.pi # for spin dynamics
-    eta=0.02
-    itr_dsf=200
+    qx=np.pi     # wave vector value q for spin dynamics  S(q, \omega)
+    eta=0.02    # Lorentzian width
+    itr_dsf=200 # iteration number for spin dynamics
 
     Nup, Nhilbert, ihfbit, irght, ilft, iup = init_parameters(N,Sz)
     binirght = np.binary_repr(irght,width=N)
@@ -338,13 +298,13 @@ def main():
     print("ilft,binilft=",ilft,binilft)
     print("iup,biniup=",iup,biniup)
     start = time.time()
-    list_1, list_ja, list_jb = make_list(N,Nup,Nhilbert,ihfbit,irght,ilft,iup)
+    list_1, list_ja, list_jb = make_list(Nup,Nhilbert,ihfbit,irght,ilft,iup)
     end = time.time()
     print (end - start)
     print("")
 
 
-    Jxx, Jzz, list_isite1, list_isite2, Nint = make_lattice_chain(N,J1,J2)#make_lattice(Lx,Ly,J1,J2)
+    Jxx, Jzz, list_isite1, list_isite2, Nint = make_lattice_chain(N,J1,J2)
     print (Jxx)
     print (Jzz)
     print (list_isite1)
