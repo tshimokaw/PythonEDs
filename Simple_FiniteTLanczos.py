578ba3899e4c4cc1607dfd440b9b32687cbc4b39
tshimokaw <tokuro-shimokawa@oist.jp>
2022-11-14 14:42:27 +0900

    Update Simple_FiniteTLanczos.py

--- Simple_FiniteTLanczos.py
+++ Simple_FiniteTLanczos.py
@@ -11,24 +11,24 @@ from matplotlib import pyplot
 def ham_to_vec_nosz_conserv(w,v1,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert):
     w = np.zeros(Nhilbert,dtype=float)
 
-    for i in range(Nhilbert):  #loop for all basis state (from 0 to 2**N-1)
-       ii = i                 #i-th basis
-       for k in range(Nint):  #loop for all interaction (Nint = # of interaction J_ij)
+    for n in range(Nhilbert):  #loop for all basis state (from 0 to 2**N-1)
+       ii = n                  #n-th basis
+       for k in range(Nint):   #loop for all interaction (Nint = # of interaction J_ij)
         isite1 = list_isite1[k] # site i for J_ij S_i S_j
         isite2 = list_isite2[k] # site j for J_ij S_i S_j
         is1 = 1<<isite1
         is2 = 1<<isite2
-        is12 = is1 + is2     #up-up spin state only at i=isite1 and j=isite2 sites
-        wght = 2.0*Jxx[k]   #The transverse J_ij
-        diag = Jzz[k]        #The longitudinal J_ij
-        ibit = ii & is12       #To check the spin config at i=isite1 and j=isite2 sites
+        is12 = is1 + is2        #up-up spin state only at i=isite1 and j=isite2 sites
+        wght = 2.0*Jxx[k]       #The transverse J_ij
+        diag = Jzz[k]           #The longitudinal J_ij
+        ibit = ii & is12        #To check the spin config at i=isite1 and j=isite2 sites
 
         if (ibit==0 or ibit==is12):   #if i and j sites have up-up or down-down spin configs.
-             w[i] += diag*v1[i]     #For S_i^z S_j^z term
-        else:                        #if i and j sites have up-down or down-up spin configs.
-            w[i] -= diag*v1[i]       #For S_i^z S_j^z term 
-            iexchg = ii ^ is12       #Flip two spins and get to know the new basis number (decinal number)
-            w[i] += wght*v1[iexchg]#For (S_i^+ S_j^- + S_i^- S_j^+) term 
+             w[n] += diag*v1[n]       #For S_i^z S_j^z term
+        else:                         #if i and j sites have up-down or down-up spin configs.
+            w[n] -= diag*v1[n]        #For S_i^z S_j^z term 
+            iexchg = ii ^ is12        #Flip two spins and get to know the new basis number (decinal number)
+            w[n] += wght*v1[iexchg]  #For (S_i^+ S_j^- + S_i^- S_j^+) term 
     return w
 
 def simple_FTL_nosz_conserv(R,M,Jxx,Jzz,list_isite1,list_isite2,N,Nint,seed0):
@@ -41,7 +41,7 @@ def simple_FTL_nosz_conserv(R,M,Jxx,Jzz,list_isite1,list_isite2,N,Nint,seed0):
                
         alphas = []                              #Diagonal parts of the trigonal matrix
         betas = [0.]                             #Off-diagonal parts of the trigonal matrix
-        v1 = (1+1)*np.random.rand(Nhilbert)-1  #old Lanczos vector (real number vector)
+        v1 = (1+1)*np.random.rand(Nhilbert)-1    #Initial random vector, |phi_0^(r)>
         v1 /= np.linalg.norm(v1)                 #normalization
         v0 = np.zeros(Nhilbert, dtype=float)     #new Lanczos vector
         w  = np.zeros(Nhilbert, dtype=float)
@@ -50,7 +50,7 @@ def simple_FTL_nosz_conserv(R,M,Jxx,Jzz,list_isite1,list_isite2,N,Nint,seed0):
         beta = 0.
         pre_energy=0
     
-        for iteration in range(0, M):
+        for a in range(0, M): # Lanczos iteration
             w = ham_to_vec_nosz_conserv(w,v1,Jxx,Jzz,list_isite1,list_isite2,N,Nint,Nhilbert)
             alpha = np.dot(v1,w)
             w = w -alpha*v1 -beta*v0
@@ -63,7 +63,7 @@ def simple_FTL_nosz_conserv(R,M,Jxx,Jzz,list_isite1,list_isite2,N,Nint,seed0):
         t_eigs,t_vecs = scipy.linalg.eigh_tridiagonal(alphas,betas[1:-1])
         epsilons[r,:]=t_eigs[:]/4 
         minene = min(t_eigs)/4
-        v1psi[r,:]=t_vecs[0,:] #1st component of the eigenvector, <V_r | Psi_j^r>
+        v1psi[r,:]=t_vecs[0,:] # The 0-th component of the eigenvector, <V_0^(r)|phi_a^(r)>
 
     return epsilons,v1psi,minene
 
@@ -78,9 +78,9 @@ def calc_Tdep_ene(R,M,epsilons,v1psi,minene):
          ene = 0.0
 
          for r in range(R):
-             for i0 in range(M):
-                 PartZ += np.exp(-beta*epsilons0[r,i0])*abs(v1psi[r,i0])**2      
-                 ene  +=  epsilons0[r,i0]*np.exp(-beta*epsilons0[r,i0])*abs(v1psi[r,i0])**2
+             for a in range(M):
+                 PartZ += np.exp(-beta*epsilons0[r,a])*abs(v1psi[r,a])**2      
+                 ene  +=  epsilons0[r,a]*np.exp(-beta*epsilons0[r,a])*abs(v1psi[r,a])**2
 
          TdepEne[T0,:] =T, ene/PartZ+minene 
 
@@ -97,9 +97,9 @@ def calc_Tdep_C(R,M,epsilons,v1psi,minene,TdepEne):
         C = 0.0
 
         for r in range(R):
-            for i0 in range(M):
-                PartZ += np.exp(-beta*epsilons0[r,i0])*abs(v1psi[r,i0])**2      
-                C  +=  (abs(epsilons[r,i0])**2)*np.exp(-beta*epsilons0[r,i0])*abs(v1psi[r,i0])**2
+            for a in range(M):
+                PartZ += np.exp(-beta*epsilons0[r,a])*abs(v1psi[r,a])**2      
+                C  +=  (abs(epsilons[r,a])**2)*np.exp(-beta*epsilons0[r,a])*abs(v1psi[r,a])**2
 
         TdepC[T0,:] = T, C/PartZ/T/T - abs(TdepEne[T0,1])**2/T/T 
 
